1b6abc3dd5b5334468a3439ab9879e3711939643
Higor <higorvmonteiro@gmail.com>
2024-11-07 16:52:39 -0300

    backend: don't allow the user to share with themselves

--- backend/apps/tasks/services.py
+++ backend/apps/tasks/services.py
@@ -1,9 +1,14 @@
+from django.core.exceptions import ValidationError
+
 from apps.tasks.models import Task
 
 
 def create_task(
     user, title, description=None, due_date=None, category=None, shared_with=None
 ):
+
+    if shared_with and user in shared_with:
+        raise ValidationError("You cannot share a task with yourself.")
 
     task = Task.objects.create(
         title=title,
@@ -14,6 +19,7 @@ def create_task(
     )
 
     if shared_with:
+        shared_with = [u for u in shared_with if u != user]
         task.shared_with.set(shared_with)
 
     return task
--- backend/tests/test_services.py
+++ backend/tests/test_services.py
@@ -1,7 +1,9 @@
 import datetime
 
+from django.forms import ValidationError
 import pytest
 from django.contrib.auth.models import User
+from apps.tasks.models import Task
 from model_bakery import baker
 
 from apps.tasks.services import create_task
@@ -23,7 +25,6 @@ class TestServices:
             category=category,
             shared_with=shared_users,
         )
-
         assert task.title == "Service Task"
         assert task.description == "New task"
         assert task.category == category
@@ -32,3 +33,25 @@ class TestServices:
             assert shared_user in task.shared_with.all()
 
         assert len(task.shared_with.all()) == len(shared_users)
+
+    def test_create_task_service_with_self(self, user, category):
+        with pytest.raises(ValidationError, match="You cannot share a task with yourself."):
+            create_task(
+                user,
+                "Service Task",
+                description="New task",
+                category=category,
+                shared_with=[user],
+            )
+        assert Task.objects.count() == 0
+
+    def test_create_task_service_with_self_in_shared(self, user, category):
+        with pytest.raises(ValidationError, match="You cannot share a task with yourself."):
+            create_task(
+                user,
+                "Service Task",
+                description="New task",
+                category=category,
+                shared_with=[user, baker.make(User)],
+            )
+        assert Task.objects.count() == 0
