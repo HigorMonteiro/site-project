6e9ca9755ff515ab9b7e3604d0b2e3bb23595650
Higor <higorvmonteiro@gmail.com>
2024-11-09 15:46:15 -0300

    Add responsive login screen layout
    - Implement AuthLayout component with responsive design
    - Use Tailwind CSS for styling and layout
    - Include responsive grid layout with breakpoints for different screen sizes
    - Add footer that is visible on larger screens

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,9 +1,21 @@
+import { ListCheck  } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
+
 export function AuthLayout() {
-  return (
-    <div>
-      <h1>Autenticação</h1>
-      <div>
+    return (
+      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <ListCheck className="h-5 w-5" />
+          <span className="font-semibold">ToDo-List</span>
+        </div>
+
+        <footer className="text-sm hidden lg:block">
+          Sua lista &copy; ToDo-List - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,9 +1,36 @@
 import { Helmet } from 'react-helmet-async';
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
     return (
       <>
         <Helmet title="Login" />
-        <h1>SignIn</h1>
+        <div className="p-8 flex justify-center items-center min-h-screen">
+          <div className="flex w-full max-w-md flex-col justify-center gap-6">
+            <div className="flex flex-col gap-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Organize sua Vida com Facilidade
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Gerencie suas tarefas diárias com eficiência!
+              </p>
+            </div>
+            <form className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Seu e-mail</Label>
+                <Input id="email" type="email" />
+                <Label htmlFor="password">Sua senha</Label>
+                <Input id="password" type="password" />
+              </div>
+              <Button className="w-full" type="submit">
+                Acessar Lista
+              </Button>
+            </form>
+          </div>
+        </div>
       </>
-)}
+    )
+}
