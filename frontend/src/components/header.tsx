2f7df395a14ad6271b19d0f41fe9a9c209b25ae5
Higor <higorvmonteiro@gmail.com>
2024-11-09 16:08:41 -0300

    frontend: head component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,24 @@
+import { ListCheck, Home, Share2 } from 'lucide-react'
+import { NavLink } from './nav-link'
+import { Separator } from './ui/separator'
+
+export function Header() {
+    return (
+        <div className="border-b">
+            <div className="flex h-16 items-center justify-center gap-6 px-6">
+                <Home className="h-6 w-6" />
+                <Separator orientation="vertical" className="h-6" />
+                <nav className="flex items-center space-x-4 lg:space-x-6">
+                    <NavLink to="/">
+                        <ListCheck className="h-4 w-4" />
+                        Início
+                    </NavLink>
+                    <NavLink to="/">
+                        <Share2 className="h-4 w-4" />
+                        Tarefas
+                    </NavLink>
+                </nav>
+            </div>
+        </div>
+    )
+}
