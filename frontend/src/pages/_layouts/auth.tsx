0a2a5a78e832e09f3c4bf212c1a7f79c7357a69b
Higor <higorvmonteiro@gmail.com>
2024-11-09 18:46:06 -0300

    Enhance sign-up form with password validation and feedback
    - Add validation to check if passwords match before submitting the form
    - Display success message with navigation action to login page on successful sign-up
    - Display error message on sign-up failure
    - Use toast notifications for feedback

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
     return (
-      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 antialiased">
+      <div className="lg:min-h-screen grid grid-cols-1 lg:grid-cols-2 antialiased">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <ListCheck className="h-5 w-5" />
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,36 +1,73 @@
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInForm = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
-    return (
-      <>
-        <Helmet title="Login" />
-        <div className="p-8 flex justify-center items-center min-h-screen">
-          <div className="flex w-full max-w-md flex-col justify-center gap-6">
-            <div className="flex flex-col gap-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Organize sua Vida com Facilidade
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Gerencie suas tarefas diárias com eficiência!
-              </p>
-            </div>
-            <form className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="email">Seu e-mail</Label>
-                <Input id="email" type="email" />
-                <Label htmlFor="password">Sua senha</Label>
-                <Input id="password" type="password" />
-              </div>
-              <Button className="w-full" type="submit">
-                Acessar Lista
-              </Button>
-            </form>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('🎉 Login realizado com sucesso!', {
+        duration: 2000,
+        richColors: true,
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Login" />
+
+      <div className="p-8 flex justify-center items-center min-h-screen">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Sign Up</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Organize sua vida com facilidade
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Gerencie suas tarefas diárias com eficiência!
+            </p>
           </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+              <Label htmlFor="email">Sua senha</Label>
+              <Input id="password" type="password" {...register('password')} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+               Acessar Lista
+            </Button>
+          </form>
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
